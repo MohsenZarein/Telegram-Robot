deda7eac172ad2307757bfaa9eac6d92b3a90449
mohsen <mohsenzarein1379@gmail.com>
2020-07-31 01:39:58 +0430

    changed model admin and added search field for members and workers models

--- bot/admin.py
+++ bot/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Target_Groups , Source_Groups , Members , Workers
 
+
+class BotAdminMembers(admin.ModelAdmin):
+    search_fields = ('member_id','member_access_hash','adding_permision','member_username','member_joined_groups',)
+    list_display = ('member_id','member_username',)
+    list_display_links = ('member_id',)
+    list_per_page = 100
+
+
+class BotAdminWorkers(admin.ModelAdmin):
+    search_fields = ('worker_api_id','worker_api_hash','worker_phone',)
+    list_display = ('worker_api_id','worker_api_hash','worker_phone',)
+    list_display_links = ('worker_phone',)
+    list_per_page = 100
+
+
 admin.site.register(Target_Groups)
 admin.site.register(Source_Groups)
-admin.site.register(Members)
-admin.site.register(Workers)
+admin.site.register(Members , BotAdminMembers)
+admin.site.register(Workers , BotAdminWorkers)
