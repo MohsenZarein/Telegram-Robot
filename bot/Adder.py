e378d3cb1d4289940de55d28e28365777f65a874
mohsen <mohsenzarein1379@gmail.com>
2020-08-12 19:02:14 +0430

    seprated add and scrap scripts / double try for connecting clients on both scripts

---
+++ bot/Adder.py
@@ -0,0 +1,192 @@
+from telethon import TelegramClient
+from telethon.tl.functions.messages import ImportChatInviteRequest , AddChatUserRequest 
+from telethon.tl.types import InputPeerEmpty , InputPeerChannel  , InputUser , InputPeerChat
+from telethon.tl.functions.channels import InviteToChannelRequest , JoinChannelRequest
+from telethon.errors import PeerFloodError , UserPrivacyRestrictedError , ChatAdminRequiredError , FloodWaitError
+
+from .models import Source_Groups
+from .models import Target_Groups
+from .models import Workers
+from .models import Members
+
+from time import sleep
+import threading
+import datetime
+import logging
+import socks
+import sys
+import random
+
+
+
+""" Logging Configuration """
+
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger('telethon').setLevel(level=logging.ERROR)
+
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+
+""" End Configuration """
+
+
+
+def Add_Members_To_Target_Groups(worker , group , members_list):
+
+    logger.info('START ADDING TO {0}...'.format(group))
+    sleep(5)
+    try:
+        try:
+            client = TelegramClient(
+                                    worker.worker_phone,
+                                    worker.worker_api_id,
+                                    worker.worker_api_hash
+            )
+            client.connect()
+            if not client.is_user_authorized():
+                logger.error('worker with {0} api_id is not signed in , skiping this worker'.format(worker.worker_api_id))
+                return
+        except:
+            sleep(7)
+            try:
+                client = TelegramClient(
+                                    worker.worker_phone,
+                                    worker.worker_api_id,
+                                    worker.worker_api_hash
+                )
+                client.connect()
+                if not client.is_user_authorized():
+                    logger.error('worker with {0} api_id is not signed in , skiping this worker'.format(worker.worker_api_id))
+                    return
+            except Exception as err:
+                logger.error(err)
+                return
+                
+
+        chat_status = ""
+        try:
+            group_entity = client.get_entity(group)
+            sleep(random.randrange(120,200))
+            client(JoinChannelRequest(group_entity))
+            logger.info('Joined source channel')
+            chat_status = "public"
+            sleep(random.randrange(120,200))
+        except Exception:
+            try:
+                client(ImportChatInviteRequest(group.split('/')[-1]))
+                logger.info('Joined source chat')
+                sleep(random.randrange(300,350))
+                group_entity = client.get_entity(group)
+                chat_status = "private"
+                sleep(random.randrange(120,200))
+
+            except Exception as err:
+                logger.error(err)
+                logger.error('Skipping This Worker...')
+                client.disconnect()
+                return
+        
+        target = InputPeerChannel(
+                                int(group_entity.id),
+                                int(group_entity.access_hash)
+        )
+
+        max_retry_for_peerflood = 7
+
+        for member in members_list:
+    
+            try:
+                logger.info("Adding {0}  {1} ...".format(int(member.member_id),member.member_username))
+                
+                user_ready_to_add = InputUser(
+                                              user_id=int(member.member_id),
+                                              access_hash=int(member.member_access_hash)
+                )
+
+                client(InviteToChannelRequest(
+                                             target,
+                                             [user_ready_to_add]
+                ))
+                
+                this_member = Members.objects.get(member_id=member.member_id)
+                this_member.member_joined_groups.append(group)
+                this_member.save()
+
+                max_retry_for_peerflood = 7
+
+                logger.info("User Added ... going to sleep for 900-1000 sec")
+                sleep(random.randrange(900,1000))
+                
+            except PeerFloodError:
+                logger.info("Peer flood error ! Too many requests on destination server !")
+                logger.info('Going for 1000 - 1100 sec sleep')
+                max_retry_for_peerflood = max_retry_for_peerflood - 1
+                if max_retry_for_peerflood <= 0 :
+                    logger.error("This worker is limited ... Returned")
+                    this_worker = Workers.objects.get(worker_api_hash=worker.worker_api_hash)
+                    this_worker.limited = True
+                    this_worker.save()
+                    return
+                sleep(random.randrange(1000,1100))
+                continue
+            except FloodWaitError as err:
+                logger.error('Something wrong with the server , Have to sleep ' + err.seconds + ' seconds')
+                sleep(err.seconds)
+                continue
+            except UserPrivacyRestrictedError:
+                logger.info("This user's privacy does not allow you to do this ... Skipping this user")
+                logger.info("Going for 900-1000 sec sleep")
+                this_member = Members.objects.get(member_id=member.member_id)
+                this_member.adding_permision = False
+                this_member.save()
+                max_retry_for_peerflood = 7
+                sleep(random.randrange(800,900))
+                continue
+            except Exception as err:
+                logger.error(err)
+                try:
+                    user_ready_to_add = InputUser(
+                        user_id=int(member.member_id),
+                        access_hash=int(member.member_access_hash)
+                    )
+                    chat_target = InputPeerChat(int(group_entity.chat_id))
+                    client(AddChatUserRequest(
+                                              chat_target,
+                                              user_ready_to_add,
+                                              fwd_limit=100
+                    ))
+                    this_member = Members.objects.get(member_id=member.member_id)
+                    this_member.member_joined_groups.append(group)
+                    this_member.save()
+                    logger.info("User Added ... going to sleep for 900-1000 sec")
+                    sleep(random.randrange(900,1000))
+                except Exception as err:
+                    logger.error(err)
+                    logger.error("Going for 800-900 sec sleep")
+                    sleep(random.randrange(800,900))
+                    continue
+
+        logger.info('Action completed')
+        client.disconnect()
+    
+
+    except ConnectionError as err:
+        logger.error(err)
+        logger.info('----Use a proxy or VPN-----')
+        return
+    except KeyboardInterrupt:
+        print('keyboard interupt...')
+        client.disconnect()
+        return
+    except Exception as err:
+        logger.error(err)
+        client.disconnect()
+        return
---
+++ bot/Scraper.py
@@ -0,0 +1,268 @@
+from telethon import TelegramClient
+from telethon.tl.functions.messages import CheckChatInviteRequest , ImportChatInviteRequest , AddChatUserRequest 
+from telethon.tl.types import UserStatusLastMonth  , ChannelParticipantsSearch 
+from telethon.tl.functions.channels import JoinChannelRequest , GetParticipantsRequest , GetFullChannelRequest
+from telethon.errors import PeerFloodError , ChatAdminRequiredError , FloodWaitError ,ChannelPrivateError 
+
+from django.core import exceptions
+
+from .models import Workers
+from .models import Source_Groups
+from .models import Members
+
+from time import sleep
+import threading
+import datetime
+import logging
+import socks
+import sys
+import random
+
+
+""" Logging Configuration """
+
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger('telethon').setLevel(level=logging.ERROR)
+
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+
+""" End Configuration """
+
+
+
+FULL_MEMBER_LIST = []
+def Scraping():
+
+    global FULL_MEMBER_LIST
+
+    logger.info("START SCRAPING ...")
+    try:
+
+        try:
+            workers_list = Workers.objects.filter(limited=False)
+        except exceptions.ObjectDoesNotExist as err:
+            logger.error(err)
+            return
+        except Exception as err:
+            logger.error(err)
+            return
+
+        clients = []
+        for worker in workers_list:
+            try:
+                client = TelegramClient(
+                                        worker.worker_phone,
+                                        worker.worker_api_id,
+                                        worker.worker_api_hash
+                )
+                client.connect()
+                if not client.is_user_authorized():
+                    logger.error('worker with {0} api_id is not signed in , skiping this worker'.format(worker.worker_api_id))
+                    continue
+                
+                if client.is_connected():
+                    worker_and_client_obj = [client,worker]
+                    clients.append(worker_and_client_obj)
+                    logger.info('a client connected...')
+                    sleep(5)
+            except:
+                sleep(7)
+                try:
+                    client = TelegramClient(
+                                        worker.worker_phone,
+                                        worker.worker_api_id,
+                                        worker.worker_api_hash
+                    )
+                    client.connect()
+                    if not client.is_user_authorized():
+                        logger.error('worker with {0} api_id is not signed in , skiping this worker'.format(worker.worker_api_id))
+                        continue
+                    if client.is_connected():
+                        worker_and_client_obj = [client,worker]
+                        clients.append(worker_and_client_obj)
+                        logger.info('a client connected...')
+                        sleep(5)
+                except Exception as err:
+                    logger.error(err)
+                    continue
+
+        
+        if not clients:
+            logger.error('None of clients connected ! try again...')
+            return
+        
+        logger.info('some clients connected successfuly ! ...')
+        sleep(10)
+
+        
+        try:
+            groups = Source_Groups.objects.all()
+        except exceptions.ObjectDoesNotExist:
+            logger.error('There is not any source groups in database !')
+            for i in range(len(clients)):
+                clients[i][0].disconnect()
+                sleep(1)
+            return
+        except Exception as err:
+            logger.error(err)
+            for i in range(len(clients)):
+                clients[i][0].disconnect()
+                sleep(1)
+            return
+        
+        for group in groups:
+        
+            data = clients[0][0](GetFullChannelRequest(group.link))
+            limit = int(data.full_chat.participants_count / len(clients))
+            
+            if limit > 180:
+                limit = 180
+
+            print('limit :',limit)
+            
+            #workers_threads = []
+            for i in range(len(clients)):
+
+                th = threading.Thread(target=Scrap , args=(clients[i],group))
+                th.start()
+                th.join()
+
+
+                
+                #for worker in workers_threads:
+                    #worker.start()
+                    #sleep(1)
+                
+
+                #for worker in workers_threads:
+                    #worker.join()
+                
+
+                logger.info('Now saving members of {0} into database ...'.format(group.link))
+            
+                counter = 0
+                for member in FULL_MEMBER_LIST[2]:
+                    if counter < limit:
+                        if not Members.objects.filter(member_id=member.id).exists():
+                            if member.username:
+                                username = member.username
+                            else:
+                                username = ""
+                            a_member = Members.objects.create(
+                                                            member_id=member.id,
+                                                            member_access_hash=member.access_hash,
+                                                            member_username=username,
+                                                            member_source_group=FULL_MEMBER_LIST[1],
+                                                            scraped_by=FULL_MEMBER_LIST[0]
+                            )
+                            a_member.save()
+                            counter = counter + 1
+                        
+
+                    else: 
+                        break
+                
+                if counter < limit :
+                    break 
+
+                FULL_MEMBER_LIST = []
+                sleep(15)
+
+            logger.info('saved successfuly!')
+            logger.info('Going to sleep for 120 sec')
+            sleep(120)
+            logger.info('1 group complted ....')
+
+        
+        logger.info('Action completed !')
+        Source_Groups.objects.all().delete()
+
+        for i in range(len(clients)):
+            clients[i][0].disconnect()
+            sleep(1)
+
+
+    except ConnectionError as err:
+        logger.error(err)
+        logger.info('----Use a proxy or VPN-----')
+        return
+    except KeyboardInterrupt:
+        for i in range(len(clients)):
+            clients[i][0].disconnect()
+            sleep(1)
+        logger.error('EXITED')
+        return
+    except Exception as err:
+        logger.error(err)
+        logger.error('EXITED')
+        for i in range(len(clients)):
+            clients[i][0].disconnect()
+            sleep(1)
+        return
+
+        
+
+
+
+def Scrap(client , group):
+
+    global FULL_MEMBER_LIST
+
+    logger.info('Getting entity of {0}'.format(group.link))
+    try:
+        g_entity = client[0].get_entity(group.link)
+        sleep(random.randrange(80,110))
+        client[0](JoinChannelRequest(g_entity))
+        logger.info('Joined source channel')
+        sleep(random.randrange(80,110))
+    except Exception:
+        try:
+            client[0](ImportChatInviteRequest(group.link.split('/')[-1]))
+            logger.info('Joined source chat')
+            sleep(random.randrange(80,110))
+            g_entity = client[0].get_entity(group.link)
+            sleep(random.randrange(80,110))
+
+        except Exception as err:
+            logger.error(err)
+            return
+            
+    
+    logger.info("Getting members from {0} ...".format(g_entity.title))
+
+    try:        
+        
+        all_participants = client[0].get_participants(g_entity, aggressive=True)
+        #FULL_MEMBER_LIST.append([client[1] , group.link , all_participants])
+        FULL_MEMBER_LIST.append(client[1])
+        FULL_MEMBER_LIST.append(group.link)
+        FULL_MEMBER_LIST.append(all_participants)
+
+        print(len(all_participants))
+        logger.info('Members scraped successfully !')
+
+    except ChatAdminRequiredError:
+        logger.error('Chat admin privileges does not allow you to scrape members ... Skipping this group')
+        return
+    except FloodWaitError as err:
+        logger.error('Something wrong with the server , Have to sleep ' + err.seconds + ' seconds')
+        sleep(err.seconds)
+        return
+    except PeerFloodError as err:
+        logger.info(err)
+        logger.info('Going for 300-350 sec sleep')
+        sleep(random.randrange(300,350))
+        return
+    except Exception as err:
+        logger.error('Unexpected error while scraping ... Skipping this group')
+        logger.error(err)
+        return
